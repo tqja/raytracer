94215c3bd3cd6ebb7926483604d9d0e80c62d118
Tariq Ameer <1tariqameer@gmail.com>
2025-09-17 00:35:58 +0100

    add exceptions for constructor + refactor

--- src/Framebuffer.cpp
+++ src/Framebuffer.cpp
@@ -2,14 +2,33 @@
 #include "colour.h"
 #include "Point.h"
 #include <vector>
+#include <limits>
 
-Framebuffer::Framebuffer(int h, int w)
-    : height{ h }, width{ w }, pixels(h * w)
+Framebuffer::Framebuffer(int w, int h)
+    : width{ w }, height{ h }
 {
-    init_default();
+    if (h <= 0) {
+        throw std::invalid_argument("Framebuffer height must be greater than 0");
+    }
+
+    if (w <= 0) {
+        throw std::invalid_argument("Framebuffer width must be greater than 0");
+    }
+
+    if (h > max_height) {
+        throw std::invalid_argument("Framebuffer height must be less than max_height");
+    }
+
+    if (w > max_width) {
+        throw std::invalid_argument("Framebuffer width must be less than max_width");
+    }
+
+    pixels.resize(width * height);
+
+    //InitDefault();
 }
 
-void Framebuffer::init_default() {
+void Framebuffer::InitDefault() {
     for (int y = 0; y < height; y++) {
         float b = 0.5f;
         std::clog << "\rScanlines remaining: " << (height - y) << ' ' << std::flush;
@@ -24,12 +43,12 @@ void Framebuffer::init_default() {
                 static_cast<int>(255.999 * b)
             );
 
-            set_pixel_colour(pixel_colour, Point(x, y));
+            SetPixelColour(pixel_colour, Point(x, y));
         }
     }
 }
 
-void Framebuffer::set_pixel_colour(const colour pixel_colour, const Point p) {
+void Framebuffer::SetPixelColour(const colour pixel_colour, const Point p) {
     const int row_offset = p.y * width;
     pixels[row_offset + p.x] = pixel_colour;
 }
