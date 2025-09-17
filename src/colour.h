3eeb221e8952b547a1f7c2377aa2f3a284da0a1b
Tariq Ameer <1tariqameer@gmail.com>
2025-09-17 05:08:52 +0100

    add assert for range constraint in constructor

--- src/colour.h
+++ src/colour.h
@@ -3,5 +3,16 @@
 #include <vector>
 #include "vec3.h"
 
-using Colour = vec3;
+#include <cassert>
+
+class Colour : public vec3 {
+public:
+    Colour() : vec3(0, 0, 0) {}
+    Colour(double r, double g, double b) : vec3(r, g, b) {
+        assert(0.0 <= x() && x() < 256.0 && "red component must be in range [0.0, 256.0)");
+        assert(0.0 <= y() && y() < 256.0 && "green component must be in range [0.0, 256.0)");
+        assert(0.0 <= z() && z() < 256.0 && "blue component must be in range [0.0, 256.0)");
+    }
+};
+
 
