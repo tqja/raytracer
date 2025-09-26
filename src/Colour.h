c465eb6b1e31c66eb5a4f268b13219878c5d8960
Tariq Ameer <1tariqameer@gmail.com>
2025-09-26 21:22:18 +0100

    Revert Colour class to alias of Vec3

--- src/Colour.h
+++ src/Colour.h
@@ -5,14 +5,4 @@
 
 #include <cassert>
 
-class Colour : public Vec3 {
-public:
-    Colour() : Vec3{ 0, 0, 0 } {}
-    Colour(double r, double g, double b) : Vec3{ r, g, b } {
-        assert(0.0 <= x() && x() < 256.0 && "red component must be in range [0.0, 256.0)");
-        assert(0.0 <= y() && y() < 256.0 && "green component must be in range [0.0, 256.0)");
-        assert(0.0 <= z() && z() < 256.0 && "blue component must be in range [0.0, 256.0)");
-    }
-};
-
-
+using Colour = Vec3;
