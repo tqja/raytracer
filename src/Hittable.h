a7f16a7cd314140888c626e0e880be92cd44f6c7
Tariq Ameer <1tariqameer@gmail.com>
2025-10-03 20:20:20 +0100

    Add Hittable abstract class

---
+++ src/Hittable.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Ray.h"
+
+class HitRecord {
+public:
+    Point3 p{};
+    Vec3 normal{};
+    double t{};
+    bool front_face{};
+
+    void SetFaceNormal(const Ray& ray, const Vec3& outward_normal) {
+        front_face = dot(ray.GetDirection(), outward_normal) < 0;
+        normal = front_face ? outward_normal : - outward_normal;
+    }
+};
+
+class Hittable {
+public:
+    virtual ~Hittable() = default;
+
+    virtual bool hit(const Ray&, double ray_tmin, double ray_tmax, HitRecord& rec) const = 0;
+};
