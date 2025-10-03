c46061cb9f8238e6029680ee96f4043e3c0c8b06
Tariq Ameer <1tariqameer@gmail.com>
2025-10-03 21:22:17 +0100

    Change resolution of output image

--- src/main.cpp
+++ src/main.cpp
@@ -85,7 +85,7 @@ Framebuffer Render(int image_width, int image_height, const Camera& camera, Hitt
 
 int main() {
     constexpr float aspect_ratio{ 16.0f / 9.0f };
-    constexpr int image_width{ 1920 };
+    constexpr int image_width{ 400 };
     constexpr int image_height{ static_cast<int>(image_width / aspect_ratio) };
     constexpr double viewport_height{ 2.0 };
 
