5d377bc951d59eaeca17f540ce5b2ba2bcab3d4d
Tariq Ameer <1tariqameer@gmail.com>
2025-10-05 21:51:11 +0100

    Add Clamp() method

--- src/Interval.h
+++ src/Interval.h
@@ -15,6 +15,12 @@ public:
         return m_min < x && x < m_max;
     }
 
+    double Clamp(double x) const {
+        if (x < m_min) return m_min;
+        if (x > m_max) return m_max;
+        return x;
+    }
+
     double Min() const {
         return m_min;
     }
