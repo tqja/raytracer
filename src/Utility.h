44c123ab30291355922057b3c27fbeae5cec7f9b
Tariq Ameer <1tariqameer@gmail.com>
2025-10-05 21:51:39 +0100

    Add methods for random double generation

--- src/Utility.h
+++ src/Utility.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
-
+#include <random>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -14,4 +14,14 @@ constexpr double pi{ 3.1415926535897932385 };
 
 inline double DegToRads(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double RandomDouble() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double RandomDouble(double min, double max) {
+    return min + (max - min) * RandomDouble();
 }
