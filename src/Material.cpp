d0ed3347f4d9965ee5a55aba90c1c7e8251587ac
Tariq Ameer <1tariqameer@gmail.com>
2025-11-19 01:00:31 +0000

    Move RandomMaterial() definition to source file

--- src/Material.cpp
+++ src/Material.cpp
@@ -1,5 +1,10 @@
+#include <functional>
+#include <numeric>
+#include <vector>
+
 #include "Material.h"
 #include "ColourConstants.h"
+#include "Vec3.h"
 
 bool Lambertian::Scatter(
     const Ray& ray_in, const HitRecord& record, Colour& attenuation, Ray& scattered
@@ -55,4 +60,54 @@ float Dielectric::Reflectance(float cosine, float refraction_index) {
     float r0 = (1 - refraction_index) / (1 + refraction_index);
     r0 = r0 * r0;
     return r0 + (1 - r0) * std::powf((1 - cosine), 5);
+}
+
+shared_ptr<Material> RandomMaterial() {
+    struct MaterialChoice {
+        float weight{};
+        std::function<shared_ptr<Material>()> make;
+    };
+
+    static auto MakeDiffuse = []() {
+        Colour albedo{ Colour::Random() * Colour::Random() };
+        return make_shared<Lambertian>(albedo);
+        };
+    static auto MakeMetal = []() {
+        const float colour_low{ 0.5f };
+        const float colour_high{ 1.0f };
+        const auto albedo{ Colour::Random(colour_low, colour_high) };
+
+        const float fuzz_low{ 0.0f };
+        const float fuzz_high{ 0.5f };
+        const auto fuzz{ RandomFloat(fuzz_low, fuzz_high) };
+
+        return make_shared<Metal>(albedo, fuzz);
+        };
+    static auto MakeGlass = []() {
+        const float refraction_index{ 1.5f };
+        return make_shared<Dielectric>(refraction_index);
+        };
+
+    static const std::vector<MaterialChoice> materials{
+        {0.4f, MakeDiffuse},
+        {0.4f, MakeMetal},
+        {0.2f, MakeGlass},
+    };
+
+    static const float total_weight{
+        std::accumulate(materials.begin(), materials.end(), 0.0f,
+                [](float sum, const MaterialChoice& choice) { return sum + choice.weight; })
+    };
+
+    auto p_material{ RandomFloat() * total_weight };
+    float p_cumulative{ 0.0f };
+    for (const auto& choice : materials) {
+        p_cumulative += choice.weight;
+        if (p_material < p_cumulative) {
+            return choice.make();
+        }
+    }
+
+    // fallback - select final material in vector
+    return materials.back().make();
 }
--- src/Material.h
+++ src/Material.h
@@ -2,12 +2,7 @@
 
 #include "Hittable.h"
 #include "Ray.h"
-#include "Vec3.h"
 #include "Colour.h"
-
-#include <functional>
-#include <numeric>
-#include <vector>
 
 class Material {
 public:
@@ -58,52 +53,4 @@ private:
 };
 
 
-inline shared_ptr<Material> RandomMaterial() {
-    struct MaterialChoice {
-        float weight{};
-        std::function<shared_ptr<Material>()> make;
-    };
-
-    static auto MakeDiffuse = []() {
-        Colour albedo{ Colour::Random() * Colour::Random() };
-        return make_shared<Lambertian>(albedo);
-        };
-    static auto MakeMetal = []() {
-        const float colour_low{ 0.5f };
-        const float colour_high{ 1.0f };
-        const auto albedo{ Colour::Random(colour_low, colour_high) };
-
-        const float fuzz_low{ 0.0f };
-        const float fuzz_high{ 0.5f };
-        const auto fuzz{ RandomFloat(fuzz_low, fuzz_high) };
-
-        return make_shared<Metal>(albedo, fuzz);
-        };
-    static auto MakeGlass = []() {
-        const float refraction_index{ 1.5f };
-        return make_shared<Dielectric>(refraction_index);
-        };
-
-    static const std::vector<MaterialChoice> materials{
-        {0.4f, MakeDiffuse},
-        {0.4f, MakeMetal},
-        {0.2f, MakeGlass},
-    };
-
-    static const float total_weight{
-        std::accumulate(materials.begin(), materials.end(), 0.0f,
-                [](float sum, const MaterialChoice& choice) { return sum + choice.weight; })
-    };
-
-    auto p_material{ RandomFloat() * total_weight };
-    float p_cumulative{ 0.0f };
-    for (const auto& choice : materials) {
-        p_cumulative += choice.weight;
-        if (p_material < p_cumulative) {
-            return choice.make();
-        }
-    }
-
-    // fallback - select final material in vector
-    return materials.back().make();
-}
+shared_ptr<Material> RandomMaterial();
